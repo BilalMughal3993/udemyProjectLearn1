9cf8aff77365064db614b68cf960989b8e27c03e
BilalMughal3993 <98820231+BilalMughal3993@users.noreply.github.com>
2022-05-29 00:40:57 +0500

    applying validation using switch in color values changing

--- src/Components/ColorCounter.js
+++ src/Components/ColorCounter.js
@@ -1,12 +1,15 @@
 import { StyleSheet, Text, View ,Button} from 'react-native'
 import React from 'react'
 
+
+
+
 const ColorCounter = ({colorName,onIncrease,onDecrease}) => {
   return (
     <View>
       <Text>{colorName}</Text>
       <Button onPress={()=>{onIncrease()}} title={`Increase ${colorName}`}/>
-      <Button onPress={()=>{onIncrease()}} title={`Decrease ${colorName}`}/>
+      <Button onPress={()=>{onDecrease()}} title={`Decrease ${colorName}`}/>
     </View>
   )
 }
--- src/Screens/SquareScreen.js
+++ src/Screens/SquareScreen.js
@@ -4,25 +4,51 @@ import { StyleSheet,View,Text } from "react-native";
 
 import ColorCounter from "../Components/ColorCounter";
 
-const COLOR_VALUE=10;
+
+
+let COLOR_VALUE=10;
 const SquareScreen = ()=>{
     const [red, setRed] = useState(0);
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
+
+    const handleLimit = (color, change) => {
+        switch (color) {
+          case 'red':
+            red + change > 255 || red + change < 0 ? null : setRed(red + change);
+            return;
+          case 'green':
+            green + change > 255 || green + change < 0
+              ? null
+              : setGreen(green + change);
+            return;
+          case 'blue':
+            blue + change > 255 || blue + change < 0
+              ? null
+              : setBlue(blue + change);
+            return;
+          default:
+            return;
+        }
+      };
+
+console.log(red)
+console.log(green)
+console.log(blue)
     return(
         <View>
         <ColorCounter 
-        onIncrease={()=>{setRed(red+COLOR_VALUE)}}
-        onDecrease={()=>{setRed(red-COLOR_VALUE)}}
+        onIncrease={()=>handleLimit("red",COLOR_VALUE)}
+        onDecrease={()=>handleLimit('red',-1*COLOR_VALUE)}
         colorName="Red"
         />
         <ColorCounter 
-        onIncrease={()=>{setGreen(green+COLOR_VALUE)}}
-        onDecrease={()=>{setGreen(green-COLOR_VALUE)}}
+        onIncrease={()=>handleLimit('green',COLOR_VALUE)}
+        onDecrease={()=>handleLimit('green',-1*COLOR_VALUE)}
         colorName="Green"/>
         <ColorCounter  
-        onIncrease={()=>{setBlue(blue+COLOR_VALUE)}}
-        onDecrease={()=>{setBlue(blue-COLOR_VALUE)}}
+        onIncrease={()=>handleLimit('blue',COLOR_VALUE)}
+        onDecrease={()=>handleLimit('blue',-1*COLOR_VALUE)}
         colorName="Blue"/>
 
         <View style={{width:100,height:100,backgroundColor:`rgb(${red},${green},${blue})`}}>
